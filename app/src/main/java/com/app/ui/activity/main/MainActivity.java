f71da5dd53e137cd7e99348db1f1ad5ee061d24a
wmt <wmtemail@email.com>
2017-02-09 15:55:03 +0800

    ijkplayer modify 0.7.7.1

---
+++ app/src/main/java/com/app/ui/activity/main/MainActivity.java
@@ -0,0 +1,17 @@
+package com.app.ui.activity.main;
+
+/**
+ * project：cutv_ningbo
+ * description：
+ * create developer： admin
+ * create time：15:54
+ * modify developer：  admin
+ * modify time：15:54
+ * modify remark：
+ *
+ * @version 2.0
+ */
+
+
+public class MainActivity {
+}
